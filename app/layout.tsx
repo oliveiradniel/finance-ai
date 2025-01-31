a5ae09a3a3337ecf18099e6cb45825a323b0b9b7
oliveiradniel <kadadniel@gmail.com>
2025-01-30 21:27:48 -0300

    chore: add clerk setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 
+import { ClerkProvider } from "@clerk/nextjs";
+
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -13,7 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="dark">{children}</body>
+      <body className="dark">
+        <ClerkProvider>{children}</ClerkProvider>
+      </body>
     </html>
   );
 }
