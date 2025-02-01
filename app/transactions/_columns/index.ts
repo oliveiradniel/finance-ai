cb747c6bed40f8a731b6dcd40a642a9594c84fc3
oliveiradniel <kadadniel@gmail.com>
2025-01-31 21:05:23 -0300

    feat: add transactions page

--- app/transactions/_columns/index.ts
+++ app/transactions/_columns/index.ts
@@ -31,5 +31,6 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
   },
   {
     accessorKey: "actions",
+    header: "",
   },
 ];
---
+++ app/transactions/page.tsx
@@ -0,0 +1,20 @@
+import { db } from "../_lib/prismaClient";
+
+import { transactionColumns } from "./_columns";
+
+import { DataTable } from "../_components/ui/data-table";
+import { Button } from "../_components/ui/button";
+
+export default async function Transaction() {
+  const transactions = await db.transaction.findMany({});
+
+  return (
+    <div className="space-y-6 p-6">
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl font-bold">Transações</h1>
+        <Button className="rounded-full">Adicionar transação</Button>
+      </div>
+      <DataTable columns={transactionColumns} data={transactions} />
+    </div>
+  );
+}
