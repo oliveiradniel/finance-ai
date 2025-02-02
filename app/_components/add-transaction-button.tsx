478854de72e01c744bba81d6eb6a2ee59d5c0e9c
oliveiradniel <kadadniel@gmail.com>
2025-02-02 16:07:49 -0300

    feat: create the modal for adding transactions

--- app/_components/add-transaction-button.tsx
+++ app/_components/add-transaction-button.tsx
@@ -1,11 +1,330 @@
+"use client";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { useForm } from "react-hook-form";
+
+import {
+  TransactionCategory,
+  TransactionPaymentMethod,
+  TransactionType,
+} from "@prisma/client";
+
 import { ArrowDownUpIcon } from "lucide-react";
+
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { MoneyInput } from "./money-input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { DatePicker } from "./ui/date-picker";
+import { DialogClose } from "@radix-ui/react-dialog";
+
+const TRANSACTION_TYPE_OPTIONS = [
+  {
+    value: TransactionType.EXPENSE,
+    label: "Despesa",
+  },
+  {
+    value: TransactionType.DEPOSIT,
+    label: "Depósito",
+  },
+  {
+    value: TransactionType.INVESTMENT,
+    label: "Investimento",
+  },
+];
+
+const TRANSACTION_PAYMENT_METHOD_OPTIONS = [
+  {
+    value: TransactionPaymentMethod.BANK_TRANSFER,
+    label: "Transferência Bancária",
+  },
+  {
+    value: TransactionPaymentMethod.BANK_SLIP,
+    label: "Boleto Bancário",
+  },
+  {
+    value: TransactionPaymentMethod.CASH,
+    label: "Dinheiro",
+  },
+  {
+    value: TransactionPaymentMethod.CREDIT_CARD,
+    label: "Cartão de Crédito",
+  },
+  {
+    value: TransactionPaymentMethod.DEBIT_CARD,
+    label: "Cartão de Débito",
+  },
+  {
+    value: TransactionPaymentMethod.OTHER,
+    label: "Outros",
+  },
+  {
+    value: TransactionPaymentMethod.PIX,
+    label: "Pix",
+  },
+];
+
+const TRANSACTION_CATEGORY_OPTIONS = [
+  {
+    value: TransactionCategory.EDUCATION,
+    label: "Educação",
+  },
+  {
+    value: TransactionCategory.ENTERTAINMENT,
+    label: "Entretenimento",
+  },
+  {
+    value: TransactionCategory.FOOD,
+    label: "Comida",
+  },
+  {
+    value: TransactionCategory.HEALTH,
+    label: "Saúde",
+  },
+  {
+    value: TransactionCategory.HOUSING,
+    label: "Moradia",
+  },
+  {
+    value: TransactionCategory.OTHER,
+    label: "Outros",
+  },
+  {
+    value: TransactionCategory.SALARY,
+    label: "Pix",
+  },
+  {
+    value: TransactionCategory.TRANSPORTATION,
+    label: "Transporte",
+  },
+  {
+    value: TransactionCategory.UTILITY,
+    label: "Utilidade",
+  },
+];
+
+const formSchema = z.object({
+  name: z.string().trim().min(1, {
+    message: "O nome é obrigatório!",
+  }),
+  amount: z.string().trim().min(1, {
+    message: "O valor é obrigatório!",
+  }),
+  type: z.nativeEnum(TransactionType, {
+    required_error: "O tipo é obrigatório!",
+  }),
+  category: z.nativeEnum(TransactionCategory, {
+    required_error: "A categoria é obrigatória!",
+  }),
+  paymentMethod: z.nativeEnum(TransactionPaymentMethod, {
+    required_error: "O método de pagamento é obrigatório!",
+  }),
+  date: z.date({
+    required_error: "A data é obrigatória!",
+  }),
+});
+
+type FormSchema = z.infer<typeof formSchema>;
 
 export default function AddTransactionButton() {
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      amount: "",
+      category: TransactionCategory.OTHER,
+      date: new Date(),
+      name: "",
+      paymentMethod: TransactionPaymentMethod.CASH,
+      type: TransactionType.EXPENSE,
+    },
+  });
+
+  function onSubmit(data: FormSchema) {
+    console.log(data);
+  }
+
   return (
-    <Button className="rounded-full font-bold">
-      Adicionar transação
-      <ArrowDownUpIcon />
-    </Button>
+    <Dialog
+      onOpenChange={(open) => {
+        if (!open) {
+          form.reset();
+        }
+      }}
+    >
+      <DialogTrigger asChild>
+        <Button className="rounded-full font-bold">
+          Adicionar transação
+          <ArrowDownUpIcon />
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Adicionar transação</DialogTitle>
+          <DialogDescription>Insira as informações abaixo</DialogDescription>
+        </DialogHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nome</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Digite o nome..." {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="amount"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Valor</FormLabel>
+                  <FormControl>
+                    <MoneyInput placeholder="Digite o valor..." {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="type"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Tipo</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Selecione o tipo da transação" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {TRANSACTION_TYPE_OPTIONS.map((option) => (
+                        <SelectItem key={option.value} value={option.value}>
+                          {option.label}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="category"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Categoria</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Selecione a categoria" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {TRANSACTION_CATEGORY_OPTIONS.map((option) => (
+                        <SelectItem key={option.value} value={option.value}>
+                          {option.label}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="paymentMethod"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Método de pagamento</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Selecione um método de pagamento" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {TRANSACTION_PAYMENT_METHOD_OPTIONS.map((option) => (
+                        <SelectItem key={option.value} value={option.value}>
+                          {option.label}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="date"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Data</FormLabel>
+                  <DatePicker value={field.value} onChange={field.onChange} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <DialogFooter>
+              <DialogClose>
+                <Button type="button" variant="outline">
+                  Cancelar
+                </Button>
+              </DialogClose>
+              <Button>Adicionar</Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
   );
 }
