88fa3189df18122cc7f966c0f92e84f9d11b1756
oliveiradniel <kadadniel@gmail.com>
2025-02-02 12:42:34 -0300

    feat: create transaction add button component

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -3,7 +3,7 @@ import { db } from "../_lib/prismaClient";
 import { transactionColumns } from "./_columns";
 
 import { DataTable } from "../_components/ui/data-table";
-import { Button } from "../_components/ui/button";
+import AddTransactionButton from "../_components/add-transaction-button";
 
 export default async function Transaction() {
   const transactions = await db.transaction.findMany({});
@@ -12,7 +12,7 @@ export default async function Transaction() {
     <div className="space-y-6 p-6">
       <div className="flex w-full items-center justify-between">
         <h1 className="text-2xl font-bold">Transações</h1>
-        <Button className="rounded-full font-bold">Adicionar transação</Button>
+        <AddTransactionButton />
       </div>
       <DataTable columns={transactionColumns} data={transactions} />
     </div>
